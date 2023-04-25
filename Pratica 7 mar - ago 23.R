0e76154b1a6fa09d8d314649fae493be76a958ce
JLLivisaca <jllivisaca@users.noreply.github.com>
2023-04-25 10:55:02 -0500

    Update Pratica 7 mar - ago 23.R

--- Pratica 7 mar - ago 23.R
+++ Pratica 7 mar - ago 23.R
@@ -4,3 +4,31 @@ pt(0.4, 1, lower.tail = F)
 qt(4.9, 5, lower.tail = T)
 dt(4.9, 5) # para sacar los valores puntuales ocupo la función dt. 
 
+# Datos de la base GE 
+library(readr)
+datos <- read_csv("C:/Users/Juan Llivisaca/Downloads/GE.csv")
+set.seed(1234)
+muestra = sample(datos$`Adj Close`, 20,replace = F)
+
+# Estadística descriptiva de la población 
+summary(datos$`Adj Close`)
+library(ggplot2)
+ggplot(datos, aes(x= datos$Date, y= datos$`Adj Close` ))+
+geom_line() 
+a= mean(datos$`Adj Close`)
+abline(v= a, col= "red")
+
+# Histograma 
+library(esquisse)
+esquisser()
+
+# fecha
+
+datos$Date = as.Date(datos$Date, "%Y/%m/%D")
+
+
+# Estadística descriptiva de la muestra
+
+summary(muestra)
+
+
