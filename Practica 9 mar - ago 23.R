a7f8a5649ca0538fc07070e7189629bcf7a17d27
JLLivisaca <jllivisaca@users.noreply.github.com>
2023-05-22 15:46:55 -0500

    Create Practica 9 mar - ago 23.R

---
+++ Practica 9 mar - ago 23.R
@@ -0,0 +1,18 @@
+qnorm(0.033, mean = 0, sd = 1, lower.tail = F) # Es el valor de Zalfa
+
+# alfa = 0.022, ¿cuál es el valor de Z?
+
+# Z = 1.456, ¿cuál es el valor de 1-alfa?
+
+# si 1 - alfa = 0.8845, ¿cuál es el valor de Zalfa?
+
+
+# Cuando tengo un alfa.medios de 0.05, ¿cuál es el valor de Zalfa.medios?
+
+# Para el ejemplo planteado en clase, el intervalo bilateral para la media poblacional fue de 
+# 151.8 y 178.1. Se desea un intervalo unilateral superior, su valor sería:
+ # a) igual a 178.1 
+ # b) menor a 178.1
+ # c) mayor a 178.1 
+
+# Demuestre
